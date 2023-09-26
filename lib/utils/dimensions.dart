5342f1d2e3b90d563d6fdaa173b5031a63bd82ef
Tenki <srodriguezdev.l@gmail.com>
2023-09-26 00:36:54 -0500

    Se realiza la preparacion del proyecto, seteando los valores proximos a usar:

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,45 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double screeHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+  static double pageView = screeHeight / 2.78;
+  static double pageViewContainer = screeHeight / 4.04;
+  static double pageViewTextContainer = screeHeight / 7.4;
+
+  static double height10 = screeHeight / 89.03;
+  static double height15 = screeHeight / 59.3;
+  static double height20 = screeHeight / 44.5;
+  static double height30 = screeHeight / 29.7;
+  static double height45 = screeHeight / 19.7;
+
+  // dynamic width, margin y padding
+  static double width10 = screeHeight / 89.03;
+  static double width15 = screeHeight / 59.3;
+  static double width20 = screeHeight / 44.5;
+  static double width30 = screeHeight / 29.7;
+  static double width45 = screeHeight / 19.7;
+
+  // font Size
+  static double font20 = screeHeight / 44.5;
+  static double font16 = screeHeight / 51.2;
+  static double font26 = screeHeight / 34.2;
+
+  static double radius15 = screeHeight / 59.3;
+  static double radius20 = screeHeight / 44.5;
+  static double radius30 = screeHeight / 29.7;
+
+  //Icon Size
+  static double iconSize24 = screeHeight / 37;
+  static double iconSize16 = screeHeight / 55.6;
+
+  // List View Size
+  static double listViewImg = screenWidth / 3.7;
+  static double listViewTextContSize = screenWidth / 4.4;
+
+  // popular food
+  static double popularFoodImgSize = screeHeight / 2.5;
+
+  // bottom height
+  static double bottomHeighBar = screeHeight / 6.8; //6.8
+}
