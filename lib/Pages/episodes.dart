95d888c0bfb98a39a5a3912d63d8dc02ee1a0454
Tenki <srodriguezdev.l@gmail.com>
2023-09-26 00:45:42 -0500

    Se culmina la distribucion por capas

---
+++ lib/Pages/episodes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Episodes extends StatelessWidget {
+  const Episodes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        child: Column(
+          children: [ListTile()],
+        ),
+      ),
+    );
+  }
+}
